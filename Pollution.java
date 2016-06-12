7ccec3cfcfd76477c0c6f105149972911d6d3e8c
usabhinav <usabhinav@gmail.com>
2016-06-12 04:15:26 -0700

    Update Pollution.java

--- Pollution.java
+++ Pollution.java
@@ -24,6 +24,27 @@ public class Pollution extends AppCompatActivity {
     Button zip_butt;
     EditText edit_zip;
     String zip_str= "";
+    String[] stateURLS = new String[]{
+            "https://files.airnowtech.org/airnow/today/forecast_aqi_20160612_al_ga_fl.jpg",
+            "https://files.airnowtech.org/airnow/today/forecast_aqi_20160612_alaska.jpg",
+            "https://files.airnowtech.org/airnow/today/forecast_aqi_20160612_co_ut_az_nm.jpg",
+            "https://files.airnowtech.org/airnow/today/forecast_aqi_20160612_ar_la_ms.jpg,",
+            "https://files.airnowtech.org/airnow/today/forecast_aqi_20160612_ca_nv.jpg",
+            "https://files.airnowtech.org/airnow/today/forecast_aqi_20160612_vt_nh_ma_ct_ri_me.jpg",
+            "https://files.airnowtech.org/airnow/today/forecast_aqi_20160612_va_wv_md_de_dc.jpg",
+            "https://files.airnowtech.org/airnow/today/forecast_aqi_20160612_hawaii.jpg",
+            "https://files.airnowtech.org/airnow/today/forecast_aqi_20160612_mt_id_wy.jpg",
+            "https://files.airnowtech.org/airnow/today/forecast_aqi_20160612_ia_il_mo.jpg",
+            "https://files.airnowtech.org/airnow/today/forecast_aqi_20160612_mi_in_oh.jpg",
+            "https://files.airnowtech.org/airnow/today/forecast_aqi_20160612_ne_ks.jpg",
+            "https://files.airnowtech.org/airnow/today/forecast_aqi_20160612_ky_tn.jpg",
+            "https://files.airnowtech.org/airnow/today/forecast_aqi_20160612_maryland.jpg",
+            "https://files.airnowtech.org/airnow/today/forecast_aqi_20160612_mn_wi.jpg",
+            "https://files.airnowtech.org/airnow/today/forecast_aqi_20160612_ny_pa_nj.jpg",
+            "https://files.airnowtech.org/airnow/today/forecast_aqi_20160612_nc_sc.jpg",
+            "https://files.airnowtech.org/airnow/today/forecast_aqi_20160612_tx_ok.jpg",
+            "https://files.airnowtech.org/airnow/today/forecast_aqi_20160612_wa_or.jpg"
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

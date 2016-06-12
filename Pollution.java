1205e840d0f37909afb5c7950cb99e5333ed86e6
ananth102 <ananthbashyam1@gmail.com>
2016-06-12 00:33:25 -0700

    Update Pollution.java

--- Pollution.java
+++ Pollution.java
@@ -1,15 +1,40 @@
-package com.example.nihar.cuhacks_tds;
+package com.woof.cuhacks_dawnstar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
-public class Pollution {
-    EditText editText;
-    Button button;
-    String TAG = "gg";
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+public class Pollution extends AppCompatActivity {
+    DatabaseReference myRef = FirebaseDatabase.getInstance().getReference();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pollution);
+        myRef.push().setValue(1);
+        //myRef.child("Hi").setValue("Anant");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()){
+                    //try {
+                        Log.d("Value", (String) postSnapshot.getValue());
+                    //}catch(java.lang.ClassCastException e){
+
+                    //}
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
 }

c67d677d7c164cc9e0b7f2a38eb3decf8bc30356
ananth102 <ananthbashyam1@gmail.com>
2016-06-12 00:50:16 -0700

    Update Pollution.java

--- Pollution.java
+++ Pollution.java
@@ -3,6 +3,9 @@ package com.woof.cuhacks_dawnstar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -12,14 +15,26 @@ import com.google.firebase.database.ValueEventListener;
 
 public class Pollution extends AppCompatActivity {
     DatabaseReference myRef = FirebaseDatabase.getInstance().getReference();
+    Button zip_butt;
+    EditText edit_zip;
+    String zip_str= "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pollution);
-        myRef.push().setValue(1);
+        zip_butt = (Button) findViewById(R.id.button_zip);
+        edit_zip = (EditText) findViewById(R.id.editText_zip);
+        zip_butt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                zip_str = edit_zip.getText().toString();
+                myRef.push().setValue(zip_str);
+            }
+        });
+
         //myRef.child("Hi").setValue("Anant");
 
-        myRef.addValueEventListener(new ValueEventListener() {
+        /*myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 for (DataSnapshot postSnapshot: dataSnapshot.getChildren()){
@@ -35,6 +50,6 @@ public class Pollution extends AppCompatActivity {
             public void onCancelled(DatabaseError databaseError) {
 
             }
-        });
+        });*/
     }
 }
